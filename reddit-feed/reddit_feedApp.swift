0ed147b30b2cb8ca050ed9ba06dcf74883e0503b
Sashko Potapov <opotapov.business@gmail.com>
2024-06-02 16:40:49 +0300

    Initial Commit

---
+++ reddit-feed/reddit_feedApp.swift
@@ -0,0 +1,17 @@
+//
+//  reddit_feedApp.swift
+//  reddit-feed
+//
+//  Created by Sashko Potapov on 02.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct reddit_feedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
